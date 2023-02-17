a4f6e6adc0f4505c1cc98cd9479a6584af4c5603
Mugilan ES <mugilan.es@viewzenlabs.com>
2023-02-17 13:33:54 +0530

    big change

--- app/src/main/java/org/dhis2/usescases/crash/CrashActivity.kt
+++ app/src/main/java/org/dhis2/usescases/crash/CrashActivity.kt
@@ -144,8 +144,8 @@ fun CrashHeader() {
     ) {
         Image(
             modifier = Modifier.size(48.dp),
-            painter = painterResource(id = R.drawable.ic_dhis),
-            contentDescription = "dhis2"
+            painter = painterResource(id = R.drawable.mhssp_logo),
+            contentDescription = "mhssp"
         )
         Spacer(modifier = Modifier.width(8.dp))
         Text(
